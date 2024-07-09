b48c466a77738ded3ae11e429959047e6bb58c21
ICardosoRamos <irwyng2@gmail.com>
2024-07-09 01:51:14 -0300

    feat: arruma intellisense

--- Estacionamento.API/Data/Preco.cs
+++ Estacionamento.API/Data/Preco.cs
@@ -1,13 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
 
 namespace Estacionamento.API.Data
 {
     public class Preco
     {
-        
+
         [Key]
         public int? Id { get; set; }
 
